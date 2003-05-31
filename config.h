93b78f3fa9a8b0ab962a9dca3a61d09410e01403
Edgar Hucek <hostmaster@ed-soft.at>
2003-05-31 10:26:03 +0000

    Merged back acidental lost videochanges

--- config.h
+++ config.h
@@ -3,6 +3,6 @@
 // selects between the supported video modes, see boot.h for enum listing those available
 #define VIDEO_PREFERRED_MODE VIDEO_MODE_640x480
 
-#undef LOADHDD
-#define LOADXBE
+#undef LOADXBE
+#define LOADHDD
 
